bd69aa5e4db03c18ee3b3123e46e22c682dcb5ea
im-prasanna10 <prasannavenkatesh10.pv@gmail.com>
2024-06-18 19:43:26 +0530

    Changes

--- SwiftUITableView/ContentView.swift
+++ SwiftUITableView/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                     Text(self.itCompanies[index])
                 }
             }
-            .navigationBarTitle("List of IT Companies")
+            .navigationBarTitle("List of 'IT' Companies ")
         }
     }
 }

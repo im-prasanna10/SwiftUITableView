32b50120b3116e8e2d7672756ae096408bf337f4
im-prasanna10 <prasannavenkatesh10.pv@gmail.com>
2024-06-18 19:41:11 +0530

    Initial Commit

---
+++ SwiftUITableView/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SwiftUITableView
+//
+//  Created by Prasanna Venkatesh on 18/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var itCompanies = ["Infosys", "TCS", "Wipro", "Accenture"]
+    var body: some View {
+        NavigationView{
+            List(0..<4){
+                    index in
+                HStack{
+                    Image(self.itCompanies[index]).resizable().frame(width: 40, height: 40)
+                    Text(self.itCompanies[index])
+                }
+            }
+            .navigationBarTitle("List of IT Companies")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITableView/SwiftUITableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITableViewApp.swift
+//  SwiftUITableView
+//
+//  Created by Prasanna Venkatesh on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
